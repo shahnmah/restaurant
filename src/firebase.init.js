fc21877ed8c352838ce820df041c91f5b6bc24bc
shahnmah <shahn.mah@gmail.com>
2022-04-15 20:09:30 +0600

    resources and packages added

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvuLZhUFobWTkXku4NxHYzWbQpHptsn-4",
+  authDomain: "restaurant-ed664.firebaseapp.com",
+  projectId: "restaurant-ed664",
+  storageBucket: "restaurant-ed664.appspot.com",
+  messagingSenderId: "228652149466",
+  appId: "1:228652149466:web:15261397f06b58956653e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
